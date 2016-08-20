189603488a1783ca4e19f72112a890aed471ff2f
david <davidtnfsh@gmail.com>
2016-08-21 00:18:17 +0800

    完成爬好友的功能

--- facebook_comment.py
+++ facebook_comment.py
@@ -1,18 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import requests, json
-token = 'EAACEdEose0cBAOievnedbBoTcuQ8uZBpDWSTZBQJZCU3nZA66zTZCzzKBWI29QdLmHw1ZAOE2ZBNZCMOJQRb8pPHNslgZBJ8XhK8IdJeFoF4hYHqoXZCAmCY26NfE1pzjqIz4kjwfheGL58DEiVS7xol4zZA3vWe9jJe3H0Gh8txKcrXgZDZD'
-re = requests.get('https://graph.facebook.com/v2.5/tsaiingwen/posts?limit=1&access_token=' + token)# api用post就會顯示該使用者的貼文s
-#if you want to chose a specific time span, append 'since=1420041600' after 'post?' 
-#cause it use Unix seconds, so 1420041600 means 2015/01/01
-jsonObj=json.loads(re.text)
-with open('demo.json', 'w', encoding='utf-8') as f:
-	json.dump(jsonObj, f)
-	
-for i in jsonObj['data']:
-	# print(i)
+
+def detect_fake_user(user):
+	req_for_friendsAmount = requests.get("https://graph.facebook.com/v2.5/{0}/friends?access_token={1}".format(user['from']['id'], token))
+	friends_json = json.loads(req_for_friendsAmount.text)
+	try:
+		if friends_json['summary']['total_count']<200:
+			print(user['from']['name'], '朋友數小於200')
+		else:
+			print(user['from']['name'], '是真的帳號')
+	except Exception as e:
+		r = requests.get("https://graph.facebook.com/v2.5/{0}/?access_token={1}".format(user['from']['id'], token))
+		fj=json.loads(r.text)
+		print(fj['name'], '這個表子不讓你爬好友喔')
+
+def parse_comment(i, token):
 	req_for_posts = requests.get("https://graph.facebook.com/v2.5/{0}/comments?access_token={1}".format(i['id'], token))# api用post就會顯示該使用者的貼文的ID再加上comments就會顯示該篇貼文的評論
 	postJson = json.loads(req_for_posts.text)
+	# print(postJson)
 	for j in postJson['data']:
-		print(j)
+		# print(j)
 		req_for_comments = requests.get("https://graph.facebook.com/v2.5/{0}/comments?access_token={1}".format(j['id'], token))#用該篇評論的id再接comments就會顯示該則回應底下的所有回應
 		commentJson = json.loads(req_for_comments.text)
-		# print(commentJson, end="\n\n")
+		for k in commentJson['data']:
+			# print(k)
+			detect_fake_user(k)
+
+
+def parse_post(token):
+	re = requests.get('https://graph.facebook.com/v2.5/tsaiingwen/posts?limit=1&access_token=' + token)# api用post就會顯示該使用者的貼文s
+	#if you want to chose a specific time span, append 'since=1420041600' after 'post?' 
+	#cause it use Unix seconds, so 1420041600 means 2015/01/01
+	jsonObj=json.loads(re.text)		
+	for i in jsonObj['data']:
+		parse_comment(i, token)
+
+if __name__  ==  "__main__":
+	with open('accessToken.txt','r',encoding='utf-8') as f:
+		token = f.read()
+	parse_post(token)
